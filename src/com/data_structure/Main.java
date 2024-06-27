78e21bf49624572d1ef47d884bc2dcd3aeae1cb5
felipel7 <felipelseven@gmail.com>
2024-06-27 17:16:02 -0300

    refactor: code style

--- src/com/data_structure/Main.java
+++ src/com/data_structure/Main.java
@@ -1,25 +1,25 @@
 package com.data_structure;
 
 import com.data_structure.linkedlist.LinkedList;
-
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        LinkedList list = new LinkedList();
-        list.addLast(10);
-        list.addLast(20);
-        list.addLast(30);
-        list.removeLast();
 
-        list.addFirst(1);
-        list.removeFirst();
+  public static void main(String[] args) {
+    LinkedList list = new LinkedList();
+    list.addLast(10);
+    list.addLast(20);
+    list.addLast(30);
+    list.removeLast();
 
-        System.out.println("List size: " + list.size());
-        System.out.println("index: " + list.indexOf(10));
-        System.out.println("contains: " + list.contains(1));
+    list.addFirst(1);
+    list.removeFirst();
 
-        int[] array = list.toArray();
-        System.out.println("LL -> Array: " + Arrays.toString(array));
-    }
+    System.out.println("List size: " + list.size());
+    System.out.println("index: " + list.indexOf(10));
+    System.out.println("contains: " + list.contains(1));
+
+    int[] array = list.toArray();
+    System.out.println("LL -> Array: " + Arrays.toString(array));
+  }
 }
--- src/com/data_structure/array/Array.java
+++ src/com/data_structure/array/Array.java
@@ -1,51 +1,57 @@
 package com.data_structure.array;
 
 public class Array {
-    private int[] items;
-    private int count;
 
-    public Array(int length) {
-        items = new int[length];
-    }
+  private int[] items;
+  private int count;
 
-    public void addItem(int item) {
-        if (items.length == count) {
-            int[] newItems = new int[count * 2];
+  public Array(int length) {
+    items = new int[length];
+  }
 
-            for (int i = 0; i < count; i++)
-                newItems[i] = items[i];
+  public void addItem(int item) {
+    if (items.length == count) {
+      int[] newItems = new int[count * 2];
 
-            items = newItems;
+        for (int i = 0; i < count; i++) {
+            newItems[i] = items[i];
         }
 
-        items[count++] = item;
+      items = newItems;
     }
 
-    public void removeItemAt(int index) { // O(n)
-      if (index < 0 || index >= count)
+    items[count++] = item;
+  }
+
+  public void removeItemAt(int index) { // O(n)
+      if (index < 0 || index >= count) {
           throw new IllegalArgumentException();
+      }
 
-      for (int i = index; i < count - 1; i++)
+      for (int i = index; i < count - 1; i++) {
           items[i] = items[i + 1];
+      }
 
-      count--;
+    count--;
+  }
+
+  public int indexOf(int item) { // O(n)
+      for (int i = 0; i < count; i++) {
+          if (items[i] == item) {
+              return i;
+          }
+      }
+
+    return -1;
+  }
+
+  public void print() {
+    if (count == 0) {
+      System.out.println("Array is empty.");
+      return;
     }
 
-    public int indexOf(int item) { // O(n)
-        for (int i = 0; i < count; i++)
-            if (items[i] == item)
-                return i;
-
-        return -1;
-    }
-
-    public void print() {
-        if (count == 0) {
-            System.out.println("Array is empty.");
-            return;
-        }
-
-        for (int i = 0; i < count; i++)
-            System.out.println(items[i]);
-    }
+    for (int i = 0; i < count; i++)
+      System.out.println(items[i]);
+  }
 }
