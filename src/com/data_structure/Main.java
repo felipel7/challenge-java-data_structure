fbb4fa30a67a6c4cd4c65e4ebee9293afb933e84
felipel7 <felipelseven@gmail.com>
2024-07-31 22:22:37 -0300

    chore: heap exercise

--- src/com/data_structure/Main.java
+++ src/com/data_structure/Main.java
@@ -1,7 +1,18 @@
 package com.data_structure;
 
+import com.data_structure.non_linear.Trees.heaps.Heap;
+
 public class Main {
 
   public static void main(String[] args) {
+    Heap heap = new Heap();
+    heap.insert(10);
+    heap.insert(5);
+    heap.insert(17);
+    heap.insert(4);
+    heap.insert(22);
+
+    System.out.println(heap.remove());
+    System.out.println(heap.remove());
   }
 }
--- src/com/data_structure/non_linear/Trees/heaps/Heap.java
+++ src/com/data_structure/non_linear/Trees/heaps/Heap.java
@@ -2,4 +2,120 @@ package com.data_structure.non_linear.Trees.heaps;
 
 public class Heap {
 
+  private int[] items = new int[10];
+  private int size;
+
+  public void insert(int value) {
+    if (isFull()) {
+      throw new IllegalStateException();
+    }
+
+    items[size++] = value;
+
+    bubbleUp();
+  }
+
+  public int remove() {
+    if (isEmpty()) {
+      throw new IllegalStateException();
+    }
+
+    var root = items[0];
+    items[0] = items[--size];
+
+    bubbleDown();
+
+    return root;
+  }
+
+  public boolean isFull() {
+    return size == items.length;
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  private boolean isValidParent(int index) {
+    if (!hasLeftChild(index)) {
+      return true;
+    }
+
+    var isValid = items[index] >= leftChild(index);
+    if (!hasRightChild(index)) {
+      isValid &= items[index] >= rightChildIndex(index);
+    }
+
+    return isValid;
+  }
+
+  private int largerChildIndex(int index) {
+    if (!hasLeftChild(index)) {
+      return index;
+    }
+
+    if (!hasRightChild(index)) {
+      return leftChild(index);
+    }
+
+    return (leftChild(index) > rightChild(index)) ? leftChildIndex(index) : rightChildIndex(index);
+  }
+
+  private int leftChild(int index) {
+    return items[leftChildIndex(index)];
+  }
+
+  private boolean hasLeftChild(int index) {
+    return leftChildIndex(index) <= size;
+  }
+
+  private int leftChildIndex(int index) {
+    return index * 2 + 1;
+  }
+
+  private int rightChildIndex(int index) {
+    return index * 2 + 2;
+  }
+
+  private int rightChild(int index) {
+    return items[rightChildIndex(index)];
+  }
+
+  private boolean hasRightChild(int index) {
+    return rightChildIndex(index) <= size;
+  }
+
+  private int parent(int index) {
+    return (index - 1) / 2;
+  }
+
+  private void swap(int first, int second) {
+    var temporary = items[first];
+    items[first] = items[second];
+    items[second] = temporary;
+  }
+
+  private void bubbleUp() {
+    var index = size - 1;
+    while (index > 0 && items[index] > items[parent(index)]) {
+      swap(index, parent(index));
+      index = parent(index);
+    }
+  }
+
+  private void bubbleDown() {
+    var index = 0;
+    while (index <= size && !isValidParent(index)) {
+      var largerChildIndex = largerChildIndex(index);
+      swap(index, largerChildIndex);
+      index = largerChildIndex;
+    }
+  }
 }
+
+/*
+ Formulas:
+   left = parent * 2 + 1
+   right = parent * 2 + 2
+   parent = (index - 1) / 2
+*/
